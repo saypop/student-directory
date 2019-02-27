d2cee06db7da8792735b51b10a10966b15fd4523
saypop <tome.m.dj@gmail.com>
2019-02-27 14:35:59 +0000

    All step 8 exercises

--- directory.rb
+++ directory.rb
@@ -8,9 +8,31 @@ def input_students
   name = gets.chomp
   # while the name is not empty, repeat this code
   while !name.empty? do
+    # get hobby
+    # print "Enter hobby for #{name}: "
+    # hobby = gets.chomp
+    # get cob
+    # print "Enter country of birth for #{name}: "
+    # cob = gets.chomp
+    # get dob
+    # print "Enter date of birth for #{name}: "
+    # dob = gets.chomp
+    # get height
+    # print "Enter height for #{name}: "
+    # height = gets.chomp
     # add the student hash to the array
-    students << {name: name, cohort: :november}
-    puts "Now we have #{students.count} students"
+    print "What cohort is #{name} in? "
+    while true
+      cohort = gets.chomp
+      cohort = "november" if cohort.empty?
+      if ["january", "february", "march", "april", "may", "june", "july", "august", "september", "october", "november", "december"].include? cohort.downcase
+        break
+      else
+        print "Looks like you made a spelling mistake, please enter that month again or leave blank to use default: "
+      end
+    end
+    students << {name: name, cohort: cohort.downcase.to_sym } # , hobby: hobby, cob: cob, dob: dob, height: height }
+    puts "Now we have #{students.count} student#{"s" if students.count > 1}"
     # get another name from the user
     name = gets.chomp
   end
@@ -19,17 +41,37 @@ def input_students
 end
 # methods to print header, list, and footer
 def print_header
-  puts "The students of the Villains Academy"
-  puts "-------------"
+  puts "The students of the Villains Academy".center(50)
+  puts "-------------".center(50)
 end
-def print_list(students)
-  students.each { |student| puts "#{student[:name]} (#{student[:cohort]} cohort)" }
+def print_list(students) #, letter)
+  cohorts(students).each do |cohort|
+    i = 0
+    puts "#{cohort.upcase} COHORT".center(50)
+    while i < students.count
+      puts "#{i + 1}. #{students[i][:name]}".center(50) if students[i][:cohort] == cohort # students[i][:name][0] == letter && students[i][:name].length < 12
+      i += 1
+    end
+  end
+  # students.each.with_index(1) { |student, index| puts "#{index}. #{student[:name]} (#{student[:cohort]} cohort)" if student[:name][0] == letter && student[:name].length < 12 }
+end
+def cohorts(students)
+  cohorts = []
+  students.each { |student| cohorts << student[:cohort] unless cohorts.include? student[:cohort] }
+  cohorts.sort!
 end
 def print_footer(names)
-  puts "Overall, we have #{names.count} great students"
+  puts "Overall, we have #{names.count} great students".center(50)
+end
+def get_letter
+  print "Select a letter: "
+  return gets.chomp
 end
 # call methods
 students = input_students
-print_header
-print_list(students)
-print_footer(students)
+# letter = get_letter
+if students.count > 0
+  print_header
+  print_list(students) #, letter)
+  print_footer(students)
+end
